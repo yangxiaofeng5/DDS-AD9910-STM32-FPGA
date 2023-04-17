99423b0ed2f35c9b627ec8ed805ed67ea3291b4d
Yangwenzhao <yangxiaofeng118@gmail.com>
2023-04-17 14:55:05 +0800

    first commit

---
+++ AD9910-DDS-PS/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../AD9910-DDS-PS_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
